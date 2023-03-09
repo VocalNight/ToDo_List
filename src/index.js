6d8617036114dad511d18907314af13fb5c69fb2
Vocal <vocalnight@protonmail.com>
2023-03-09 01:29:11 -0300

    added functionally to the main buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import projectManager from './projects'
 
 let projects = projectManager.Projects();
 
+//Divs
+
+
+//Buttons
+
+const inboxBtn = document.querySelector('#inboxBtn')
+const todayBtn = document.querySelector('#todayBtn')
+const weekBtn = document.querySelector('#weekBtn')
+
+inboxBtn.addEventListener('click', () => showNotes(projects.getIndex()));
+
+todayBtn.addEventListener('click', () => showNotes(projects.getToday()));
+
+weekBtn.addEventListener('click', () => showNotes(projects.getWeek()));
+
 //Testing creation of note
 
 projects.addToday(notes('a', 'b', 'c'))
@@ -11,25 +26,31 @@ projects.addToday(notes('d', 'b', 'c'))
 projects.addToday(notes('f', 'b', 'c'))
 projects.addToday(notes('g', 'b', 'c'))
 
-console.log(projects.getToday()[0].getTitle())
+projects.addWeek(notes('n', 'b', 'c'))
+projects.addWeek(notes('l', 'b', 'c'))
+projects.addWeek(notes('p', 'b', 'c'))
+projects.addWeek(notes('z', 'b', 'c'))
+
+projects.addIndex(notes('d', 'b', 'c'))
+projects.addIndex(notes('s', 'b', 'c'))
+projects.addIndex(notes('a', 'b', 'c'))
+projects.addIndex(notes('x', 'b', 'c'))
+
 
 
 
 
 // Showing the notes on screen
-function write_letters(){
-    let items = document.querySelector('.inboxList');
-    console.log(items);
+function showNotes(project){
+    let items = document.querySelector('.notesList');
+    items.innerHTML = '';
 
-    for (let i = 0; i < projects.getToday().length; i++ ) {
+    for (let i = 0; i < project.length; i++ ) {
         let item = document.createElement("li");
-        console.log(projects.getToday()[i]);
-        item.innerHTML = projects.getToday()[i].getTitle();
-        item.onclick = () => alert(projects.getToday()[i].getTitle());
+        item.innerHTML = project[i].getTitle();
+        item.onclick = () => alert(project[i].getTitle());
         item.style.cursor = "pointer"
         items.appendChild(item);
     }
 
 }
-
-write_letters();
